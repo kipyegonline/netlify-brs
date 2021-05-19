228065e7cb2c21ab39a3466bbbe1d31c63812dab
Vince <vinnykipx@gmail.com>
2021-05-19 15:19:48 +0300

    netlify toml

--- functions/brs.js
+++ functions/brs.js
@@ -1,7 +1,16 @@
 exports.handler = async (id, fn) => {
+  console.log("Netlify funct");
   try {
     const user = await axios.get(
-      `https://brs.ecitizen.go.ke/backend/id-lookup?id_number=${id}&citizenship=citizen&first_name=${fn}`
+      `https://brs.ecitizen.go.ke/backend/id-lookup?id_number=${29216950}&citizenship=citizen&first_name=Kipyegon}`,
+      {
+        headers: {
+          /* Required for CORS support to work */
+          "Access-Control-Allow-Origin": "*",
+          /* Required for cookies, authorization headers with HTTPS */
+          "Access-Control-Allow-Credentials": true,
+        },
+      }
     );
     console.log(user);
     return { user: JSON.stringify(user) };
